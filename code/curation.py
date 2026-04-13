8864bfc7bf179322c10d6d6a68094db23ae2c94b
Cody Baker <cody.c.baker.phd@gmail.com>
2026-04-13 15:47:43 -0400

    add curation script

---
+++ code/curation.py
@@ -0,0 +1,47 @@
+import pathlib
+
+def _scrub_tsv_of_line_with_value(tsv_lines: list[str], identifying_value: str) -> list[str]:
+    """
+    Remove lines containing a specific value from a list of TSV lines.
+
+    Parameters
+    ----------
+    tsv_lines : list[str]
+        The list of lines from a TSV file.
+    identifying_value : str
+        The value to search for in the lines. Lines containing this value will be removed.
+
+    Returns
+    -------
+    list[str]
+        A new list of lines with the specified value removed.
+    """
+    corrected_lines = [line for line in tsv_lines if identifying_value not in line]
+    return corrected_lines
+
+def _run(dataset_dir: pathlib.Path) -> None:
+    dandipaths_with_values_to_fix = {
+        "sub-YutaMouse57/ses-YutaMouse57+161010/ecephys/sub-YutaMouse57_ses-YutaMouse57+161010_events.tsv": "-1596.0487999999987",
+        "sub-YutaMouse42/ses-YutaMouse42+151102/ecephys/sub-YutaMouse42_ses-YutaMouse42+151102_events.tsv": "-7798.642400000001",
+    }
+    dandipaths_to_local_paths = {
+        dandipath: dataset_dir / dandipath
+        for dandipath in dandipaths_with_values_to_fix.keys()
+    }
+
+    local_paths_with_corrected_values = {
+        dandipath: _scrub_tsv_of_line_with_value(
+            tsv_lines=dandipaths_to_local_paths[dandipath].read_text().splitlines(), identifying_value=identifying_value
+        )
+        for dandipath, identifying_value in dandipaths_with_values_to_fix.items()
+    }
+    
+    for path, corrected_lines in local_paths_with_corrected_values.items():
+        path.write_text("\n".join(corrected_lines))
+
+    a = 1
+
+
+if __name__ == '__main__':
+    dataset_dir = pathlib.Path(__file__).parent.parent
+    _run(dataset_dir=dataset_dir)
